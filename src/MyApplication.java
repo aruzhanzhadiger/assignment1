8e64cc43d36f7ed11a68dde3811c4f17b6b407cf
aruzhan <aruzhan.zhadiger@icloud.com>
2025-12-20 09:36:26 +0500

    assignment 1 start

--- src/MyApplication.java
+++ src/MyApplication.java
@@ -3,17 +3,20 @@ import models.Shape;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
+
 public class MyApplication {
     public static void main(String[] arg) throws FileNotFoundException {
         File file = new File("src/source");
         Scanner sc = new Scanner(file);
         Shape shape = new Shape();
+
         while (sc.hasNextDouble()) {
             double x = sc.nextDouble();
             double y = sc.nextDouble();
             Point point = new Point(x, y);
             shape.addPoint(point);
         }
+
         System.out.println(shape.calculatePerimeter());
         System.out.println(shape.getLongestSide());
         System.out.println(shape.getAverageSide());
--- src/models/Shape.java
+++ src/models/Shape.java
@@ -1,21 +1,26 @@
 package models;
 import java.util.ArrayList;
 public class Shape {
+
     private ArrayList<Point> points;
     public Shape() {
         points = new ArrayList<>();
     }
+
     public void addPoint(Point p) {
         points.add(p);
     }
+
     public double calculatePerimeter() {
         double sum = 0;
         for (int i = 0; i < points.size() - 1; i ++) {
             sum += points.get(i).distance(points.get(i + 1));
         }
+
         sum += points.get(points.size() - 1).distance(points.get(0));
         return sum;
     }
+
     public double getLongestSide() {
         double max = 0;
         for (int i = 0; i < points.size() - 1; i ++) {
@@ -26,6 +31,7 @@ public class Shape {
         }
         return max;
     }
+
     public double getAverageSide() {
         return calculatePerimeter() / points.size();
     }
